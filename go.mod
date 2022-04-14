4cb2ff3adc156fe6368e4397e2702a55432c430f
Xiao Zhong <xzhong.dev@gmail.com>
2022-04-13 23:35:22 -0600

    set up MySQL database and create migrations

--- go.mod
+++ go.mod
@@ -9,6 +9,9 @@ require (
 
 require (
 	github.com/agnivade/levenshtein v1.1.0 // indirect
+	github.com/go-chi/chi v1.5.4
+	github.com/go-sql-driver/mysql v1.6.0
+	github.com/golang-migrate/migrate v3.5.4+incompatible
 	github.com/gorilla/websocket v1.4.2 // indirect
 	github.com/hashicorp/golang-lru v0.5.0 // indirect
 	github.com/mitchellh/mapstructure v1.2.3 // indirect
--- server.go
+++ server.go
@@ -9,6 +9,9 @@ import (
 	"github.com/99designs/gqlgen/graphql/playground"
 	"github.com/xiaozhong21/hackernews/graph"
 	"github.com/xiaozhong21/hackernews/graph/generated"
+	database "github.com/xiaozhong21/hackernews/internal/pkg/db/mysql"
+
+	"github.com/go-chi/chi"
 )
 
 const defaultPort = "8080"
@@ -19,11 +22,14 @@ func main() {
 		port = defaultPort
 	}
 
-	srv := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: &graph.Resolver{}}))
+	router := chi.NewRouter()
 
-	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
-	http.Handle("/query", srv)
+	database.InitDB()
+	database.Migrate()
+	server := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: &graph.Resolver{}}))
+	router.Handle("/", playground.Handler("GraphQL playground", "/query"))
+	router.Handle("/query", server)
 
 	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
-	log.Fatal(http.ListenAndServe(":"+port, nil))
+	log.Fatal(http.ListenAndServe(":"+port, router))
 }
